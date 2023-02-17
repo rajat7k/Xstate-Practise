b3334b462b3ec4e3c36ab582c0d3c2b240f3e842
rajat7k <kaushikrajat777@gmail.com>
2023-02-17 18:43:58 +0530

    ..

--- src/components/ProfileDetailForm/profileDetailFormMachine.js
+++ src/components/ProfileDetailForm/profileDetailFormMachine.js
@@ -43,7 +43,6 @@ export const profileDetailMachine=
                     },
                     onError:{
                         target:".failureScreen",
-                        internal: false
                     }
 
                 },
